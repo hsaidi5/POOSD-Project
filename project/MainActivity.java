31121615f88032a61216c130c4f4e7c91bb3221b
hsaidi5 <54752554+hsaidi5@users.noreply.github.com>
2021-02-05 11:03:50 -0500

    Commit initial source code

---
+++ project/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mytimeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import java.io.*;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

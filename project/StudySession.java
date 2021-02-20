2f799dfcc45b688a9a05457cb6eb96bbd3177ecb
chrisdr6 <54637034+chrisdr6@users.noreply.github.com>
2021-02-20 00:59:54 -0500

    implemented functional stopwatch

--- project/StudySession.java
+++ project/StudySession.java
@@ -4,14 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class StudySession extends AppCompatActivity
 {
     private Button back;
+    private Button start;
+
+    private int seconds = 0;
+    private boolean running;
+    private boolean wasRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +30,7 @@ public class StudySession extends AppCompatActivity
 
         setupDropdownList();
         configureBackButton();
+        configureStartButton();
 
     }
 
@@ -42,6 +53,49 @@ public class StudySession extends AppCompatActivity
             public void onClick(View v)
             {
                 finish();
+            }
+        });
+    }
+
+    private void configureStartButton()
+    {
+        start = findViewById(R.id.button);
+
+        start.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                running = true;
+                runTimer();
+            }
+        });
+    }
+
+    private void runTimer()
+    {
+        final EditText timeText = findViewById(R.id.editTextTime);
+        final Handler handler = new Handler();
+
+        handler.post(new Runnable()
+        {
+            @Override
+
+            public void run()
+            {
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds;
+
+                String time = String.format(Locale.getDefault(), "%02d:%02d", minutes, secs);
+
+                timeText.setText(time);
+
+                if (running)
+                {
+                    seconds++;
+                }
+
+                handler.postDelayed(this, 1000);
             }
         });
     }

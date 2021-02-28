a2a05ba1eb86a6d5dda5e1567d0c05bd62b00a40
yo955153 <54690280+yo955153@users.noreply.github.com>
2021-02-27 20:32:26 -0500

    Finished remove button (Shared data broke version)

--- project/UpdateCourses.java
+++ project/UpdateCourses.java
@@ -5,7 +5,6 @@ Also allows for buttons to add and remove courses from the arraylist data struct
 Still need to allow arraylist to be accessed in different classes and allow for
 a save state of the current items in the arraylist
 
-
  */
 
 
@@ -20,19 +19,26 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class UpdateCourses extends AppCompatActivity
 {
-    ArrayList<Course> courses_data_struct = new ArrayList<Course>();
 
     private Button back;
     private Button add;
     private Button remove;
-
-    EditText course_name_textbox;
-    EditText cred_hours_textbox;
-    TextView confirm_textview;
+    private EditText course_name_textbox;
+    private EditText cred_hours_textbox;
+    private TextView confirm_textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -40,12 +46,25 @@ public class UpdateCourses extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_courses);
 
-        back = findViewById(R.id.backFromUpdate);
-        add = findViewById(R.id.addCourse);
-        remove = findViewById(R.id.removeCourse);
+        ArrayList<Course> courses_data_struct = SharedData.getCurrentCourses(this);
+        if(courses_data_struct == null)
+        {
+            courses_data_struct = new ArrayList<Course>();
+        }
+
         course_name_textbox = (EditText) findViewById(R.id.courseName);
         cred_hours_textbox = (EditText) findViewById(R.id.courseNumber);
         confirm_textview = (TextView) findViewById((R.id.confirmation));
+
+        configureAddButton(courses_data_struct);
+        configureRemoveButton(courses_data_struct);
+        configureBackButton(courses_data_struct);
+    }
+
+    // Function to add course inputted by user
+    private void configureAddButton(ArrayList<Course> courses_data_struct)
+    {
+        add = findViewById(R.id.addCourse);
 
         add.setOnClickListener(new View.OnClickListener()
         {
@@ -67,6 +86,7 @@ public class UpdateCourses extends AppCompatActivity
                     //confirm_textview.setText("Successfully added:"); // debugging process
                     c.set_name(test);
                 }
+                //Check if a valid integer was entered
 
                 if(test2.equals("") || test2.equals("Credit Hours") || test2.equals("0"))
                 {
@@ -76,17 +96,32 @@ public class UpdateCourses extends AppCompatActivity
                 else
                 {
                     //confirm_textview.setText(test2); // debugging purposes
-                    int num = Integer.parseInt(test2);
-                    c.set_cred_hours(num);
+                    try
+                    {
+                        int num = Integer.parseInt(test2);
+                        c.set_cred_hours(num);
+                    }
+
+                    catch(NumberFormatException e)
+                    {
+                        confirm_textview.setText("You didn't type a valid integer.");
+                    }
                 }
 
                 if(c.get_cred_hours() != 0 && !(c.get_name().equals("")))
                 {
                     confirm_textview.setText("Successfully added: " + c.get_name() + " with credit hours: " + c.get_cred_hours());
                     courses_data_struct.add(c);
+                    SharedData.saveCourses(getApplicationContext(), courses_data_struct);
                 }
             }
         });
+    }
+
+    // Function to remove course inputted by user
+    public void configureRemoveButton(ArrayList<Course> courses_data_struct)
+    {
+        remove = findViewById(R.id.removeCourse);
 
         remove.setOnClickListener(new View.OnClickListener()
         {
@@ -107,13 +142,12 @@ public class UpdateCourses extends AppCompatActivity
                         if(courses_data_struct.get(i).get_name().equals(name_tbox))
                         {
                             courses_data_struct.remove(i);
-                            //System.out.println("hiiiiiiii");
+                            confirm_textview.setText("Course: " + name_tbox + " has been successfully removed.");
                         }
 
                         else
                         {
-                            //System.out.println("Doesn't exist");
-                            System.out.println("Course name: " + courses_data_struct.get(i).get_name() + ", Hours: " + courses_data_struct.get(i).get_cred_hours());
+                            confirm_textview.setText("Course: " + name_tbox + " doesn't exist.");
                         }
 
                     }
@@ -121,19 +155,20 @@ public class UpdateCourses extends AppCompatActivity
                 }
             }
         });
+    }
 
+    // Function to return to the View Courses activity from the Update Courses activity
+    private void configureBackButton(ArrayList<Course> courses_data_struct)
+    {
+        back = findViewById(R.id.backFromUpdate);
 
         back.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                Intent intent = new Intent(UpdateCourses.this, MainActivity.class);
-                startActivity(intent);
+                finish();
             }
         });
-
-
     }
-
 }

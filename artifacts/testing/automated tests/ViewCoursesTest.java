2dd2a6f15608ec2c85e025615e33fb5073c72196
yo955153 <54690280+yo955153@users.noreply.github.com>
2021-03-13 16:43:13 -0500

    Add files via upload

---
+++ artifacts/testing/automated tests/ViewCoursesTest.java
@@ -0,0 +1,86 @@
+// Yoseph Hassan Automated test for view courses functionality
+
+package com.mytime;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import android.view.View;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import com.google.android.material.tabs.TabLayout;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+
+public class ViewCoursesTest
+{
+    private TextView r1_course_text;
+    private TableRow r1;
+    private TableRow r2;
+
+
+    @Test
+
+    public void main()
+    {
+        //r1_course_text = (TextView) getView().findViewById(R.id.r1_course);
+
+        Course testCourse1 = new Course();
+        Course testCourse2 = new Course();
+        testCourse1.set_cred_hours(3);
+        testCourse1.set_name("Test1");
+        testCourse2.set_cred_hours(4);
+        testCourse2.set_name("Test2");
+
+        TableRow[] row_arr = {r1, r2};
+
+        //makevisible(2, row_arr);
+
+        assertEquals(3, testCourse1.get_cred_hours(), 0);
+        assertEquals(4, testCourse2.get_cred_hours(), 0);
+        assertEquals(360.0, testCourse1.get_time_available(), 0);
+        assertEquals(540, testCourse2.get_time_available(), 0);
+        assertEquals("Equal", "Test1", testCourse1.get_name());
+        assertEquals("Equal", "Test2", testCourse2.get_name());
+        assertEquals(false, testCourse1.get_comp_stat());
+        assertEquals(false, testCourse2.get_comp_stat());
+
+        testCourse1.set_time_available(0.0);
+        assertEquals(true, testCourse1.get_comp_stat());
+
+    }
+
+
+    private void makevisible(int size, TableRow[] row_arr)
+    {
+        for(int i = 0; i < size; i++)
+        {
+            if(size >= i + 1)
+            {
+                row_arr[i].setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+}
+

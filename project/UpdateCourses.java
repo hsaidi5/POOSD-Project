c9a20c765120b955ee6ed0c208d86cf9d22cb8a6
pedronemalceff <54723731+pedronemalceff@users.noreply.github.com>
2021-03-14 23:02:21 -0400

    completed code for sprint 6 (Pedro Nemalceff)

--- project/UpdateCourses.java
+++ project/UpdateCourses.java
@@ -6,9 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 
@@ -18,6 +21,7 @@ public class UpdateCourses extends AppCompatActivity
     private Button back;
     private Button add;
     private Button remove;
+    private Spinner importanceSpinner;
     private EditText course_name_textbox;
     private EditText cred_hours_textbox;
     private TextView confirm_textview;
@@ -44,6 +48,9 @@ public class UpdateCourses extends AppCompatActivity
         configureAddButton(courses_data_struct);
         configureRemoveButton(courses_data_struct);
         configureBackButton();
+
+        //Function call implemented by Pedro Nemalceff
+        setupDropdownList();
     }
 
     // Function to add course inputted by user
@@ -59,6 +66,10 @@ public class UpdateCourses extends AppCompatActivity
                 Course c = new Course();
                 String courseName = course_name_textbox.getText().toString();
                 String credits = cred_hours_textbox.getText().toString();
+
+                //Added by Pedro Nemalceff
+                String importanceLevel = importanceSpinner.getSelectedItem().toString();
+
                 // If the info they enter isn't correct or non existent
                 if( courseName.equals("") || courseName.equals("Course Name") )
                 {
@@ -86,6 +97,18 @@ public class UpdateCourses extends AppCompatActivity
                         c.set_name(courseName);
                     }
                 }
+
+                // If the info they enter isn't correct or non existent
+                //Added by Pedro Nemalceff
+                if(importanceLevel.equals("Select Importance Level") )
+                {
+                    confirm_textview.setText("Please Select an Importance Level!");
+                }
+                else
+                {
+                    c.set_importance_level(importanceLevel);
+                }
+
                 //Check if a valid integer was entered
 
                 if(credits.equals("") || credits.equals("Credit Hours") || credits.equals("0"))
@@ -107,9 +130,9 @@ public class UpdateCourses extends AppCompatActivity
                     }
                 }
 
-                if(c.get_cred_hours() != 0 && !(c.get_name().equals("")) && (exists == false))
+                if(c.get_cred_hours() != 0 && !(c.get_name().equals("")) && (exists == false) && (!importanceLevel.equals("Select Importance Level")))
                 {
-                    confirm_textview.setText("Successfully added: " + c.get_name() + " with credit hours: " + c.get_cred_hours() + "with time available: "+ c.get_time_available() + " minutes");
+                    confirm_textview.setText("Successfully added: " + c.get_name() + " with credit hours: " + c.get_cred_hours() + " with time available: "+ c.get_time_available() + " minutes with importance level: " + c.get_level_of_importance());
                     courses_data_struct.add(c);
                     SharedData.saveCourses(getApplicationContext(), courses_data_struct);
                 }
@@ -220,6 +243,28 @@ public class UpdateCourses extends AppCompatActivity
             time_arr[i].setText(time);
         }
     }*/
+
+    //Implemented by Pedro Nemalceff
+    // Dropdown list that displays the levels of importance of each course.
+    private void setupDropdownList()
+    {
+        importanceSpinner = (Spinner) findViewById(R.id.selectLevelOfImportance);
+        ArrayList<String> importanceLevels = new ArrayList<String>();
+
+        importanceLevels.add("Select Importance Level");
+        importanceLevels.add("Not Important");
+        importanceLevels.add("Somewhat Important");
+        importanceLevels.add("Important");
+        importanceLevels.add("Very Important");
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(UpdateCourses.this, android.R.layout.simple_list_item_1,
+                importanceLevels);
+
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        importanceSpinner.setAdapter(myAdapter);
+
+    }
+
 
     // Function to return to the View Courses activity from the Update Courses activity
     private void configureBackButton()

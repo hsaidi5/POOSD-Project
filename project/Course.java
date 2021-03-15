627992feda740229b97c171d3b7d64021af9e51e
Jean Cestin <54861919+Blackjean122@users.noreply.github.com>
2021-03-14 21:43:10 -0400

    Add files via upload

--- project/Course.java
+++ project/Course.java
@@ -9,11 +9,19 @@ public class Course
     private String name = "";
     private int cred_hours = 0;
     private double time_available = 0.0;
+    private boolean completion_stat = false;
+    private String level_of_importance = "";
 
     public void set_name(String str)
     {
 
         this.name = str;
+    }
+
+    //Added by Pedro Nemalceff
+    public void set_importance_level(String str)
+    {
+        this.level_of_importance = str;
     }
 
     public void set_cred_hours(int hours)
@@ -26,6 +34,11 @@ public class Course
         // subtracting 180 minutes from overall time to account for the lectures and labs (on average about 3 hours total)
         double time_after_lectures = time - 180;
         this.time_available = time_after_lectures;
+    }
+
+    public void set_comp_stat(boolean comp)
+    {
+        this.completion_stat = comp;
     }
 
     public void set_time_available(double time)
@@ -46,6 +59,26 @@ public class Course
     public double get_time_available()
     {
         return this.time_available;
+    }
+
+    //Added by Pedro Nemalceff
+    public String get_level_of_importance()
+    {
+        return this.level_of_importance;
+    }
+
+    public boolean get_comp_stat()
+    {
+        if(time_available <= 0)
+        {
+            return this.completion_stat = true;
+        }
+
+        else
+        {
+            return this.completion_stat = false;
+        }
+        //return this.completion_stat;
     }
 
 }

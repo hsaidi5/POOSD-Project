1bc2c9f6d58540ade15b0f07d6b6a658cefc3522
yo955153 <54690280+yo955153@users.noreply.github.com>
2021-03-21 16:35:57 -0400

    Added code changed during sprint 7

--- project/HistoryActivity.java
+++ project/HistoryActivity.java
@@ -6,34 +6,64 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class HistoryActivity extends AppCompatActivity
 {
 
     private Button back;
-    private Button HistoryCourses;
+    private ScrollView history_scroll;
+    private TextView historyText;
+    private ArrayList<Course> courses_data_struct = new ArrayList<Course>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
-        
-        configureHistoryCourses();
+        history_scroll = (ScrollView) findViewById(R.id.scrollView2);
+        historyText = (TextView) findViewById(R.id.historyText);
+        courses_data_struct = SharedData.getCurrentCourses(this);
+
+        populateHistoryList();
+        configureCourses(courses_data_struct);
         configureBackButton();
     }
-    
-    //Function to view the History of the Courses
-    private void configurationHistoryCourses(){
-         HistoryCourses = findViewByID(R.id.HistoryCourses);
-         HistoryCourses.setOnClickListener(new View.OnClickListener(){
-         
-             @Override
-             public void OnClick(View v){
-                 Intent intent = new Intent(ViewCourses.this.HistoryCourses.class);
-                 startActivity(intent);
-             }
-         });
+
+    private void populateHistoryList()
+    {
+        String str = new String();
+
+        for(int i = 0; i < courses_data_struct.size(); i++)
+        {
+            for(int j = 0; j < courses_data_struct.get(i).get_hours_spent().size(); j++)
+            {
+                str += "Course: " + courses_data_struct.get(i).get_name() + ", Time Spent: " + courses_data_struct.get(i).get_hours_spent().get(j) + "\n";
+            }
+            historyText.setText(str);
+        }
+    }
+
+    // TEMPORARY FUNCTION
+    private void configureCourses(ArrayList<Course> enrolledCourses)
+    {
+        TextView courseOne = findViewById(R.id.textView5);
+        TextView courseTwo = findViewById(R.id.textView4);
+
+        TextView avgOne = findViewById(R.id.textView3);
+        TextView avgTwo = findViewById(R.id.textView6);
+
+        courseOne.setText(enrolledCourses.get(0).get_name());
+        courseTwo.setText(enrolledCourses.get(1).get_name());
+
+        String timeOne = String.format(Locale.getDefault(), "%02.02f", enrolledCourses.get(0).get_avg_study_time());
+        String timeTwo = String.format(Locale.getDefault(), "%02.02f", enrolledCourses.get(1).get_avg_study_time());
+        avgOne.setText(timeOne);
+        avgTwo.setText(timeTwo);
     }
 
     // Function to return to the home page from the History activity
